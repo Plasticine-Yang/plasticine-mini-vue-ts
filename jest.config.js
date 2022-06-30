92752fff9c328aa56b98a06de2b346bb91d2e259
plasticine9750 <975036719@qq.com>
2022-06-30 16:46:25 +0800

    chore(jest): 添加模块名映射，让jest支持跨模块之间导入

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,10 @@ module.exports = {
   testEnvironment: 'node',
   watchPathIgnorePatterns: ['/node_modules/', '/dist/', '/.git/'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+  // 配置跨模块之间导入时的映射关系
+  moduleNameMapper: {
+    '^@plasticine-mini-vue-ts/(.*?)$': '<rootDir>/packages/$1/src'
+  },
   rootDir: __dirname,
   testMatch: ['<rootDir>/packages/**/__test__/**/*.spec.[jt]s?(x)'],
   testPathIgnorePatterns: ['/node_modules/']
