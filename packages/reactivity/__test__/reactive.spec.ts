41eb4150468cb4936ad888e26adc433de692ec32
plasticine9750 <975036719@qq.com>
2022-07-02 13:48:48 +0800

    test(reactive): nested reactive

--- packages/reactivity/__test__/reactive.spec.ts
+++ packages/reactivity/__test__/reactive.spec.ts
@@ -15,6 +15,19 @@ describe('reactivity/reactive', () => {
     expect(Object.keys(observed)).toEqual(['foo'])
   })
 
+  test('nested reactive', () => {
+    const original = {
+      nested: {
+        foo: 1
+      },
+      array: [{ bar: 2 }]
+    }
+    const observed = reactive(original)
+    expect(isReactive(observed.nested)).toBe(true)
+    expect(isReactive(observed.array)).toBe(true)
+    expect(isReactive(observed.array[0])).toBe(true)
+  })
+
   test('toRaw', () => {
     const original = { foo: 1 }
     const observed = reactive(original)
