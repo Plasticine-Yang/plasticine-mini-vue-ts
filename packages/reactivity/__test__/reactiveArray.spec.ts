2398549fd5c418cd624f77e541971422f13052d0
plasticine9750 <975036719@qq.com>
2022-07-07 10:45:42 +0800

    test(reactivearray): should make Array reactive

---
+++ packages/reactivity/__test__/reactiveArray.spec.ts
@@ -0,0 +1,18 @@
+import { isReactive, reactive } from '../src/reactive'
+
+describe('reactivity/reactive/Array', () => {
+  test('should make Array reactive', () => {
+    const original = [{ foo: 1 }]
+    const observed = reactive(original)
+    expect(observed).not.toBe(original)
+    expect(isReactive(observed)).toBe(true)
+    expect(isReactive(original)).toBe(false)
+    expect(isReactive(observed[0])).toBe(true)
+    // get
+    expect(observed[0].foo).toBe(1)
+    // has
+    expect(0 in observed).toBe(true)
+    // ownKeys
+    expect(Object.keys(observed)).toEqual(['0'])
+  })
+})
